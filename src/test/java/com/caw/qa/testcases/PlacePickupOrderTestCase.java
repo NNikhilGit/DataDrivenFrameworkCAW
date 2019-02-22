e22cc398889750fc6736a3f9403729785c8b2ea9
NNikhilGit <43199548+NNikhilGit@users.noreply.github.com>
2019-02-22 16:12:31 +0530

    update for New menupage changes

--- src/test/java/com/caw/qa/testcases/PlacePickupOrderTestCase.java
+++ src/test/java/com/caw/qa/testcases/PlacePickupOrderTestCase.java
@@ -1,7 +1,7 @@
 package com.caw.qa.testcases;
 
-
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -55,29 +55,28 @@ public class PlacePickupOrderTestCase extends TestBase {
 
 	}
 
-
 	@Test(dataProvider = "getPickupOrderDetails", enabled = true)
 	public void placePickupOrderAsGuestUserWithGiftCard(String locationName, String orderType, String menuname,
-			String itemname, String addonName, String optionName, String tipAmount, String fname, String lname,
-			String emailId, String cnfEmail, String phNo, String mobNo, String faxNo, String comment,
-			String paymentType, String cardNum, String expDate, String firstName, String lastName, String cvv,
-			String zipcode, String giftcardNum, String giftPinNum, String billingadd1, String stateName, String cityb,
-			String zipc) throws InterruptedException {
+			String itemname, String sizeName, String addonName, String optionName1, String optionName2,
+			String optionName3, String optionName4, String optionName5, String optionName6, String tipAmount,
+			String fname, String lname, String emailId, String cnfEmail, String phNo, String mobNo, String faxNo,
+			String comment, String paymentType, String cardNum, String expDate, String firstName, String lastName,
+			String cvv, String zipcode, String giftcardNum, String giftPinNum, String billingadd1, String stateName,
+			String cityb, String zipc) throws InterruptedException {
 		checkoutpage.goToLocationsPage();
 		locationPage.selectPickupOrder(locationName, orderType);
-		menupage.selectItemAndModifiers(menuname, itemname, addonName, optionName);
+		menupage.selectItemAndModifiers(menuname, itemname, sizeName, addonName, optionName1, optionName2, optionName3,
+				optionName4, optionName5, optionName6);
 		checkoutpage.doCheckOut();
 		checkoutpage.selectTipAmount(tipAmount);
 		checkoutpage.fillCustomerAddress(fname, lname, emailId, cnfEmail, phNo, mobNo, faxNo, comment);
-		checkoutpage.makePaymentForGuestUsers(paymentType, cardNum, expDate, firstName, lastName, cvv, zipcode, giftcardNum,
-				giftPinNum, billingadd1, stateName, cityb, zipc);
+		checkoutpage.makePaymentForGuestUsers(paymentType, cardNum, expDate, firstName, lastName, cvv, zipcode,
+				giftcardNum, giftPinNum, billingadd1, stateName, cityb, zipc);
 
 	}
 
-
-
 	@Test(enabled = false)
-	public void placeorderByLoginUser() {
+	public void placeorderByLoginUser() throws InterruptedException {
 
 		loginpage.login("nikhil.kale@bnt-soft.com", "password");
 
@@ -85,11 +84,16 @@ public class PlacePickupOrderTestCase extends TestBase {
 
 	@Test(enabled = false)
 	public void checkSocialLinks() {
-		
+
 		homepage.goToLocationPage();
 		String titleofLink = locationPage.checkSocialLink("RolyPoly Aundh", "Instagram");
 		System.out.println(titleofLink);
 		Assert.assertEquals(titleofLink, "Login • Instagram");
-		
+
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
 	}
 }
